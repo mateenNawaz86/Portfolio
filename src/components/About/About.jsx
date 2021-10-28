b7d7b67da9ad4917e20b9be655c58a67c582a9b2
Mateen Mirani <mateenmirani125@gmail.com>
2021-10-28 10:49:48 +0500

    Complete design about-section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,72 @@
 import React from "react";
-import aboutImg from "../../img/about-img.jpg";
+import { Link } from "react-router-dom";
+import aboutImg from "../../img/about-img.png";
 
 import "./About.css";
 
 const About = () => {
   return (
     <>
-      <div className="about-container">
-        <div className="about-left">
-          <div className="card-wrapper bg"></div>
-          <div className="card-wrapper">
-            <img src={aboutImg} alt="About profile" className="about-img" />
+      <section className="about-section">
+        <div className="container-row">
+          <div className="section-title">
+            <h2>About Me</h2>
           </div>
         </div>
-        <div className="about-right"></div>
-      </div>
+
+        <div className="inner-container">
+          <div className="about-img">
+            <div className="img-box inner-shadow">
+              <img
+                src={aboutImg}
+                alt="About profile"
+                className="outer-shadow"
+              />
+            </div>
+
+            <div className="social-links">
+              <Link
+                to="https://www.linkedin.com/in/mateen-mirani-2904aa167/"
+                target="_blank"
+                className="outer-shadow hover-in-shadow links"
+              >
+                <i className="fab fa-linkedin-in" />
+              </Link>
+              <Link
+                to="https://www.linkedin.com/in/mateen-mirani-2904aa167/"
+                target="_blank"
+                className="outer-shadow hover-in-shadow links"
+              >
+                <i className="fab fa-github" />
+              </Link>
+            </div>
+          </div>
+          <div className="about-info">
+            <p>
+              <span>
+                Hi! My name is Mateen Nawaz. I'm a passionate Front-end Web
+                Developer.
+              </span>{" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis voluptas suscipit quisquam? Fugiat iusto veniam ex
+              libero cum? Quidem, consequuntur!
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Exercitationem facilis nostrum iusto amet deserunt in cumque nobis
+              asperiores blanditiis magnam aut esse eligendi optio distinctio,
+              numquam minima perferendis eum debitis et recusandae. Officiis est
+              mollitia, expedita vero iste quo, repellendus molestiae vel ea
+              fugit autem placeat et optio illum aperiam!
+            </p>
+
+            <Link to="/contact" className="btn-1 outer-shadow hover-in-shadow">
+              Hire Me
+            </Link>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
